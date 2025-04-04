54e12556df4663dfa0aa8ab183abd3925744461f
Vheüel <vheuels@gmail.com>
2025-04-05 02:54:50 +0700

    Update timeline.tsx

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -8,7 +8,6 @@ import { ComposeNote } from "./compose-note";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-
 type NostrEvent = {
   id: string;
   pubkey: string;
