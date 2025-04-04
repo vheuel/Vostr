d5b9ae54810cf4bcb1eb2d0a89bcaf180ded44c7
Vheüel <vheuels@gmail.com>
2025-04-05 04:02:10 +0700

    Update note.tsx

--- components/note.tsx
+++ components/note.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 
 type NoteProps = {
@@ -19,10 +21,10 @@ const Note: React.FC<{ note: NoteProps }> = ({ note }) => {
   }
 
   return (
-    <div className="border p-4 rounded-lg shadow-md bg-white my-2">
-      <p className="text-sm text-gray-600">{note.pubkey?.substring(0, 10)}...</p>
-      <p className="text-lg font-semibold">{note.content}</p>
-      <p className="text-xs text-gray-500">{formatDate(note.created_at)}</p>
+    <div className="border p-4 rounded-lg shadow-md bg-white dark:bg-gray-900 my-2">
+      <p className="text-sm text-gray-600 dark:text-gray-400">{note.pubkey.substring(0, 10)}...</p>
+      <p className="text-lg font-semibold text-gray-900 dark:text-white">{note.content}</p>
+      <p className="text-xs text-gray-500 dark:text-gray-400">{formatDate(note.created_at)}</p>
     </div>
   );
 };
