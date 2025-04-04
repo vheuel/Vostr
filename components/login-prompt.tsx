32978b847e36acb57c095a4e5b933f73927f7d24
Vheüel <vheuels@gmail.com>
2025-04-05 03:02:09 +0700

    Update login-prompt.tsx

--- components/login-prompt.tsx
+++ components/login-prompt.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useState } from "react"
-import { useNostr } from "./nostr-provider"
+import { useNostr } from "@/components/nostr-provider"
 import { nip19 } from "nostr-tools"
 import { useToast } from "@/hooks/use-toast"
 import { useRouter } from "next/navigation"
